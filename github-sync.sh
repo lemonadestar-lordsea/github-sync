e77063f7757ad1d657fc7ccb66e5fe9e8efb178d
lemonadestar-lordsea <106223111+lemonadestar-lordsea@users.noreply.github.com>
2022-07-26 18:58:17 +0800

    Update github-sync.sh

--- github-sync.sh
+++ github-sync.sh
@@ -28,7 +28,9 @@ echo "BRANCHES=$BRANCH_MAPPING"
 
 git config --unset-all http."https://github.com/".extraheader || :
 
+pwd
 ls -la
+whoami
 git remote --verbose
 
 echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
